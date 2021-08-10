33b622bd49bd6e3e2f21fec6492b8747561e5211
Shlomi Fish <shlomif@shlomifish.org>
2021-08-10 14:11:26 +0300

    more

---
+++ to-asciidoc.pl
@@ -0,0 +1,24 @@
+#! /usr/bin/env perl
+#
+# Short description for to-asciidoc.pl
+#
+# Version 0.0.1
+# Copyright (C) 2021 Shlomi Fish < https://www.shlomifish.org/ >
+#
+# Licensed under the terms of the MIT license.
+
+use strict;
+use warnings;
+use 5.014;
+use autodie;
+
+use Path::Tiny qw/ path tempdir tempfile cwd /;
+
+path("article/summary.asciidoc")->spew_utf8(
+    sub {
+        my $t = path("article/summary.txt")->slurp_utf8();
+        $t =~ s#^( +)([\-\*])#"*" x (length($1)/4)#egms;
+        return $t;
+    }
+        ->()
+);
